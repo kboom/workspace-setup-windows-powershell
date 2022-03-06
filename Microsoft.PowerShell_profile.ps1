b0b96a2692f7202da41015ebfb51445ecec1058c
Grzegorz Gurgul <ggurgul@microsoft.com>
2022-03-06 20:18:46 +0100

    Sudo

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
 # This file initializes your profile on every session startup
 Import-Module -Name Terminal-Icons
 Set-PoshPrompt -Theme "$PSScriptRoot\ggurgul-oh-my-posh-profile.json"
+Set-Alias -Name n -Value navi
+$env:Path += ";${PSScriptRoot}\Commands"
